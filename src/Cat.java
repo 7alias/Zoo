72388d6e1f4fb330b972cd18aa13e21bac752642
Ilya Maximov <xc.navi@gmail.com>
2022-01-10 14:16:19 +0200

    3

---
+++ src/Cat.java
@@ -0,0 +1,6 @@
+public class Cat extends Animal {
+    @Override
+    public void move() {
+        System.out.println("Кошка умеет двигаться очень тихо");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Cat cat = new Cat();
+
+        animal.move();  // Запускает метод в классе Animal
+        cat.move();  // Запускает метод в классе Cat
+    }
 }
