56f700e329111c38a872d9843db5ed2dbf1167ec
Ilya Maximov <xc.navi@gmail.com>
2022-01-18 12:32:11 +0200

    Zoo test

--- src/Animal.java
+++ src/Animal.java
@@ -1,7 +1,8 @@
-public class Animal {
+abstract public class Animal {
     public void print(){
         System.out.println(this);
     }
+    abstract public void sound();
 
     @Override
     public String toString() {
